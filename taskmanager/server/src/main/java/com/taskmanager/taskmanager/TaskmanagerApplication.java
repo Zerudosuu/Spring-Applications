a9437088a5dfd59e1a1296b1372c110567fdeb82
Ronald Salvador <ronsalvador.m@gmail.com>
2026-05-09 02:38:30 +0800

    Activate dev profile and enable .env import
    Set spring.profiles.active=dev in application.properties and uncomment spring.config.import to allow loading an optional .env(.properties) file for environment overrides. Also added an import for org.springframework.test.context.ActiveProfiles in TaskmanagerApplication (preparing for profile usage).

--- taskmanager/server/src/main/java/com/taskmanager/taskmanager/TaskmanagerApplication.java
+++ taskmanager/server/src/main/java/com/taskmanager/taskmanager/TaskmanagerApplication.java
@@ -3,6 +3,7 @@ package com.taskmanager.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.test.context.ActiveProfiles;
 
 @EnableAsync
 @SpringBootApplication
