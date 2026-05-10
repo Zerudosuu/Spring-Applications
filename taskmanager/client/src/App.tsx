8cf7c699d9537e571eb7e6675e3df08e6302130a
Ronald Salvador <ronsalvador.m@gmail.com>
2026-05-10 13:46:39 +0800

    Remove unused DashboardPage import
    Delete the unused DashboardPage import from App.tsx to clean up the code and avoid unused-import/compile warnings. No functional changes intended; the Dashboard page import was not referenced in this file.

--- taskmanager/client/src/App.tsx
+++ taskmanager/client/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import DashboardPage from "./pages/DashboardPage";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import NotFoundPage from "./pages/NotFoundPage";
 import Layout from "./components/layout/Layout";
